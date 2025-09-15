c22ca7008c6401a64007e4308a48bddaa8c5e0d0
yofrelopez <c.yofrelopez@gmail.com>
2025-09-15 16:53:16 -0500

    menu de servicios actualizado 5

--- components/servicios/layout/Hero.tsx
+++ components/servicios/layout/Hero.tsx
@@ -13,7 +13,7 @@ export default function HeroService({ title, subtitle, image }: HeroServiceProps
   return (
     <section className="relative w-full h-[60vh] md:h-[70vh] flex items-center">
       <Image src={image} alt={title} fill priority className="object-cover brightness-50" />
-      <div className="relative z-10 min-w-7xl mx-auto px-6 md:px-12 text-left">
+      <div className="relative z-10 lg:min-w-7xl mx-auto px-6 md:px-12 text-left">
         <h1 className="text-4xl md:text-6xl font-bold text-white">
           <span className="border-b-4 border-yellow-400 pb-3">{first}</span>{" "}
           {rest.join(" ")}
