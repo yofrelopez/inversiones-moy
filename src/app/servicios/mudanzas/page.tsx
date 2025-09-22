19eeac4543875410560f55b0d7c6e81b872e516a
yofrelopez <c.yofrelopez@gmail.com>
2025-09-21 20:29:05 -0500

    feat: actualiza layout con íconos que se habían perido y estilos de mudanzas

--- src/app/servicios/mudanzas/page.tsx
+++ src/app/servicios/mudanzas/page.tsx
@@ -1,9 +1,21 @@
 // crea una página básica 
 
+import AlwaysOnTime from "../../../../components/servicios/mudanzas/AlwaysOnTime";
+import ContactBand from "../../../../components/servicios/mudanzas/ContactBand";
+import FeatureCards from "../../../../components/servicios/mudanzas/FeatureCards";
+import Hero from "../../../../components/servicios/mudanzas/Hero";
+import Newsletter from "../../../../components/servicios/mudanzas/Newsletter";
+
 export default function MudanzasPage() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <h1 className="text-4xl font-bold text-gray-900">Mudanzas</h1>
-        </div>
+        <div>
+        <Hero />
+        <AlwaysOnTime />
+        <Newsletter />
+        <FeatureCards />
+        <ContactBand />
+               </div>
+
+
     );
 }
