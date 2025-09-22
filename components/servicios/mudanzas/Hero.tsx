68199867d9310cdf114786c381d12c754b4d146d
yofrelopez <c.yofrelopez@gmail.com>
2025-09-21 20:07:30 -0500

    feat: actualizar Hero en sección mudanzas

--- components/servicios/mudanzas/Hero.tsx
+++ components/servicios/mudanzas/Hero.tsx
@@ -1,13 +1,14 @@
 // components/servicios/mudanzas/Hero.tsx
 import HeroService from "../layout/Hero";
-import heroImg from "../../../public/images/services/mudanzas/mudanzas-hero.jpg";
+
 
 export default function Hero() {
   return (
-    <HeroService
-      title="Mudanzas Profesionales"
-      subtitle="Con más 25 años de experiencia, garantizamos traslados seguros, rápidos y eficientes para hogares y empresas en todo el Perú."
-      image={heroImg}
-    />
+  <HeroService
+    title="Mudanzas Profesionales"
+    subtitle="Con más 25 años de experiencia, garantizamos traslados seguros, rápidos y eficientes para hogares y empresas en todo el Perú."
+    image="/images/services/mudanzas/mudanzas-hero.jpg"
+  />
+
   );
 }
